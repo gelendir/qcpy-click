05e8e59ac008174c5246bec2e389fe08270fc179
Gregory Eric Sanderson <gregory.eric.sanderson@gmail.com>
2014-07-22 16:19:15 -0400

    add hello world example

---
+++ hello.py
@@ -0,0 +1,18 @@
+import click
+
+
+@click.command()
+@click.argument('name')
+@click.option('--age', default=0, help='Your age')
+def hello(name, age):
+    click.echo("Hello {name} !".format(name=name))
+
+    if 0 < age < 42:
+        click.echo("You're young !")
+    elif age == 42:
+        click.echo("You have the universal age")
+    elif age > 42:
+        click.echo("You're old !")
+
+if __name__ == "__main__":
+    hello()
